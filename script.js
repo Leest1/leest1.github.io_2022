c0f94dfd059409395972b8dac95a3bd529182501
Shannon Lee <leest1@uci.edu>
2016-12-31 15:38:01 -0800

    more navigation bar
    drops down navigation bar when moused over
    hides bar when mouse exits

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 var scrollPosition;
 var scrollDirection;
+var prevMouseY = 100;
 
-$(document).ready(function() {
-    $(window).scroll(function() {
-        var nextPosition = $(this).scrollTop();
-        if(scrollPosition < nextPosition) {
-            if(scrollDirection != "down") {
-                $("ul.navigation").dequeue()
-                    .stop()
-                    .slideUp("slow");
-                scrollDirection = "down";
-            }
+// show navigation bar when moving mouse to top of screen ; hides when leaves top of screen
+$(document).mousemove( function(event) {
+    if(event.clientY < 50) {
+        if(prevMouseY > 50) {
+            $("ul.navigation").dequeue().stop().slideDown("slow");
         }
-        else {
-            if(scrollDirection != "up") {
-                $("ul.navigation").dequeue()
-                    .stop()
-                    .slideDown("slow");
-                scrollDirection = "up";
-            }
+    }
+    else if (event.clientY >= 50) {
+        if(prevMouseY <= 50) {
+            $("ul.navigation").dequeue().stop().slideUp("slow");
         }
-        scrollPosition = nextPosition;
-    });
+    }
+    prevMouseY = event.clientY;
 });
+
+// hide navigation bar when scrolling down; show when scroll up
+$(window).scroll(function() {
+    var nextPosition = $(this).scrollTop();
+    if(scrollPosition < nextPosition) {
+        if(scrollDirection != "down") {
+            $("ul.navigation").dequeue().stop().slideUp("slow");
+            scrollDirection = "down";
+        }
+    }
+    else {
+        if(scrollDirection != "up") {
+            $("ul.navigation").dequeue().stop().slideDown("slow");
+            scrollDirection = "up";
+        }
+    }
+    scrollPosition = nextPosition;
+});
+
+// hide navigation bar when clicking a button
+
+
+// toggle dropdown menu
+//$("#about").click( function() {
+//    $(this).hide();
+////    $(div.dropMenu).fadeToggle("slow");
+//});
