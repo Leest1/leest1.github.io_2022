bdb56c6aa7b24f15caae0235761a00f42dbc20db
Leest1 <shannyonl@gmail.com>
2022-07-02 00:54:05 -0700

    pradohealth, profile picture animation, color iteration, copy + download feature

--- jquery.js
+++ jquery.js
@@ -6,9 +6,19 @@ function contentHeight() {
     $(".background-line").css("min-height", headerHei + footerHei + contentHei);
 }
 
+function copyToClipboard(copyString) {
+    navigator.clipboard.writeText(copyString);
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied!";
+}
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
+
 // SETS UP HIDDEN ITEMS BEFORE PAGE LOAD
 $(window).bind('setup', function() {
-    // hide Looking for Work banner if user has closed before
+    // hide 'Looking for Work banner' if user has closed before
     if (sessionStorage.bannerClose == 'true') {
         $(".banner").hide();
     } else {
@@ -19,10 +29,10 @@ $(window).bind('setup', function() {
 $(document).ready(function(){
     $(window).trigger('setup');
 
-    // resize background height according to content;
+    // RESIZE BACKGROUND
     contentHeight();
 
-    // Hamburger menu toggle
+    // HAMBURGER TOGGLE
     $("#menu-toggle").on('click', function() {
         $("#primary-nav-list").slideToggle();
     });
@@ -34,7 +44,7 @@ $(document).ready(function(){
     });
 
 
-    // icon animations
+    // ICON ANIMATIONS
     $(".logo").hover(function() {
         $(".fa-seedling").addClass('fa-bounce');
     }, function() {
@@ -58,6 +68,32 @@ $(document).ready(function(){
     }, function() {
         $(this).removeClass('fa-bounce');
     });
+
+    // PROFILE PICTURE ANIMATE
+    const profilePic = document.querySelector('#about-img');
+    let range=48;
+    function shadow(e) {
+        let { offsetX: x, offsetY: y } = e;
+        if (this !== e.target) {
+            console.log(e);
+            x = x + e.target.offsetLeft;
+            y = y + e.target.offsetTop;
+            console.log(x);
+            console.log(y);
+        }
+
+        // height and width of div.hero
+        const { offsetWidth: width, offsetHeight: height } = profilePic;
+
+        const xRange = Math.round( (x/width * range)  - range/2 );
+        const yRange = Math.round( (y/height * range) - range/2 );
+        
+        profilePic.style.boxShadow = `${xRange}px ${yRange}px 0 #AF9696`;
+    }
+
+    if (profilePic) {
+        profilePic.addEventListener('mousemove', shadow);
+    }
 });
 
 $(window).resize(function() {
