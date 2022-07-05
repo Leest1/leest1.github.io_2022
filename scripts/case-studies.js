ac728b275b568c1c802f6229a8f788c214f59ad7
Leest1 <shannyonl@gmail.com>
2022-07-05 02:36:25 -0700

    search feature

--- scripts/case-studies.js
+++ scripts/case-studies.js
@@ -1,15 +1,20 @@
-// load case studies JSON into case-studies.html
+var allStudies = [];
+var currentStudies = [];
+
+// SETUP: load case studies JSON into case-studies.html
 $.getJSON("../../json/case-studies.json", function(json) {
     var caseStudies = json['caseStudies'];
 
     for (let i in caseStudies) {
         let caseStudy = caseStudies[i];
+        allStudies.push(caseStudy);
+        currentStudies.push(caseStudy);
         // create tools used string
         let toolsUsed = caseStudy.tools.join(', ');
         
 
         $('#case-studies').append(
-            $("<div class='card-large'>"+
+            $("<div class='card-large' id='"+ caseStudy.id +"'>"+
             "<img src='"+ caseStudy.img +"' alt='"+ caseStudy.imgAlt +"'>"+
             "<div class='card-large-contents'>" +
             "<h1>" + caseStudy.name + "</h1>" +
@@ -20,4 +25,45 @@ $.getJSON("../../json/case-studies.json", function(json) {
             "</div></div></div>")
         );
     }
+});
+
+// remove all case studies
+function clearCaseStudies() {
+    for (let i in currentStudies) {
+        let id = '#' + currentStudies[i].id;
+        $(id).remove();
+    }
+}
+
+// append a case study
+function addCaseStudy(caseStudy) {
+    let toolsUsed = caseStudy.tools.join(', '); 
+    $('#case-studies').append(
+        $("<div class='card-large' id='"+ caseStudy.id +"'>"+
+        "<img src='"+ caseStudy.img +"' alt='"+ caseStudy.imgAlt +"'>"+
+        "<div class='card-large-contents'>" +
+        "<h1>" + caseStudy.name + "</h1>" +
+        "<p>" + caseStudy.summary + "</p>" +
+        "<p class='tools-used'><small>TOOLS USED: " + toolsUsed + "</small></p>" +
+        "<div class='text-align-center'><a target='_blank' rel='noopener noreferrer' class='cta-link btn-default' href='"
+        + caseStudy.url +"'>READ MORE</a>" +
+        "</div></div></div>")
+    );
+}
+
+$(document).ready(function () {
+    $('#case-study-search').keyup(function () {
+        clearCaseStudies();
+
+        let inputVal = $('#case-study-search').val();
+        for (let i in allStudies) {
+            let current = allStudies[i];
+            for (let tagsIndex in current.tags) {
+                if (current.tags[tagsIndex].toLowerCase().includes(inputVal.toLowerCase())) {
+                    addCaseStudy(current);
+                    break;
+                }
+            }
+        }
+    });
 });
